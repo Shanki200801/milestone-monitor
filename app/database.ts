accb350da4ea1a8713980b6516842cb83f40f449
Shanki200801 <shashank200801@gmail.com>
2023-07-27 10:27:38 +0530

    ....

---
+++ app/database.ts
@@ -0,0 +1,27 @@
+'use client'
+
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { useEffect, useState } from 'react'
+//fetch items
+export async function getAllStaff(){
+    const supabase = createClientComponentClient()
+    const {data, error} = await supabase.from('faculty').select('*')
+    if(error) {
+        throw error
+    }
+    return data
+}
+export async function getStaff(department: string) {
+    const supabase = createClientComponentClient()
+    const {data, error} = await supabase.from('faculty').select('*').eq('staff_department', department)
+    if(error) {
+        throw error
+    }
+    return data
+
+}
+
+
+//insert items
+
+//delete items
