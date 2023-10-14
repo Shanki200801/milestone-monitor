7b49fa3123d0040b903c76932b938344e4a93dcc
programmingMohit <programming.mohit@gmail.com>
2023-10-14 10:47:15 +0530

    added add new workshop section

--- app/(categories)/my-workshops/MyWorkshops.tsx
+++ app/(categories)/my-workshops/MyWorkshops.tsx
@@ -6,7 +6,7 @@ const tableFont = Alata({weight: "400", subsets: ['latin'], })
 const MyWorkshops = () => {
   return (
   <div>
-    <div className="bg-[#cbfef8] m-3 p-5 sm:rounded">
+    <section className="bg-[#cbfef8] m-3 p-5 sm:rounded min-h-[25rem]">
       <div className="overflow-x-auto shadow-md sm:rounded">
           <table className={`${tableFont.className} w-full text-sm text-left text-black`}>
               <thead className="text-lg text-black uppercase bg-[#60fbdf]">
@@ -34,7 +34,7 @@ const MyWorkshops = () => {
                           
                       </th>
                       <td className="px-6 py-4">
-                            
+
                       </td>
                       <td className="px-6 py-4">
                           
@@ -49,7 +49,15 @@ const MyWorkshops = () => {
               </tbody>
           </table>
       </div>
-    </div>
+    </section>
+    <section className={`${tableFont.className} bg-[#cbfef8] m-3 p-5 sm:rounded flex flex-col items-center justify-center`}>
+      <p className="text-2xl">Add a new Workshop</p>
+      <div className="flex justify-self-center">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-28 h-28">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+        </svg>
+      </div>
+    </section>
   </div>
   );
 };
