8b2371288c8e6258944c67843106bb7a847289cd
Adelicia Sequeira <a.seq2804@gmail.com>
2023-10-18 08:37:52 +0530

    minor cleanup to categories group layout

--- app/(categories)/layout.tsx
+++ app/(categories)/layout.tsx
@@ -1,19 +1,5 @@
 import "../globals.css";
-import Image from "next/image";
-import logoImg from "../../public/logo.webp";
-import sjuLogo from "../../public/sju-logo.webp";
-import { Poppins } from "next/font/google";
 import NavTwo from "@/components/nav/NavTwo";
-
-const bodyText = Poppins({
-  weight: "400",
-  subsets: ["latin"],
-});
-
-export const metadata = {
-  title: "Milestone Monitor",
-  description: "A cataloguing app",
-};
 
 export default function RootLayout({
   children,
@@ -23,7 +9,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {/* Navigation Bars (horizontal and vertical) */}
+        {/* Navigation Bar (vertical) */}
         <NavTwo/>
         <main className="absolute inset-x-[2%] sm:right-0 sm:left-[10%] ">
           {children}
