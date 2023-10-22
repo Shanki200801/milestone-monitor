798ae8ba8d0d406def1ffcf9d61006440e57ebfc
Adelicia Sequeira <a.seq2804@gmail.com>
2023-10-22 09:47:56 +0530

    remaining merge.... ?

--- app/api/dbfunctions.tsx
+++ app/api/dbfunctions.tsx
@@ -211,6 +211,199 @@ export const addPatent = async (
   console.log("error is " + error?.message);
 };
 
+export const fetchData = async (tableName: string, email: string) => {
+  const supabase = createServerComponentClient({ cookies });
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  // fetches the user data of the user in session
+  const { data: userData, error: userError } = await supabase
+    .from("faculty")
+    .select()
+    .eq("faculty_email", user?.email);
+
+  //fetches the data of the user using the faculty id of userData
+  const { data: tableData, error: tableError } = await supabase
+    .from(tableName)
+    .select()
+    .eq("faculty_id", userData ? userData[0].faculty_id : "");
+
+  return tableData;
+};
+
+export const updateConf = async (
+  type: string,
+  paper_title: string,
+  conf_date: string,
+  proceedings: boolean,
+  conf_name: string,
+  proceeding_fp: boolean,
+  certificate: string,
+  id: number,
+  is_verified: string
+) => {
+  const supabase = createServerComponentClient({ cookies });
+
+  if (is_verified.toLowerCase() == "pending") {
+    const { error } = await supabase
+      .from("conferences")
+      .update({
+        paper_title: paper_title,
+        conf_name: conf_name,
+        conf_date: conf_date,
+        proceedings: proceedings,
+        certificate: certificate,
+        proceeding_fp: proceeding_fp,
+      })
+      .eq("id", id);
+  } else {
+    const { error } = await supabase
+      .from("conferences")
+      .update({
+        paper_title: paper_title,
+        conf_name: conf_name,
+        conf_date: conf_date,
+        proceedings: proceedings,
+        certificate: certificate,
+        proceeding_fp: proceeding_fp,
+        is_verified: "Pending",
+      })
+      .eq("id", id);
+  }
+};
+
+export const updateJournals = async (
+  paper_title: string,
+  date_of_publication: string,
+  journal_name: string,
+  issn_number: string,
+  indexed_in: string,
+  link: string,
+  is_verified: string,
+  id: number
+) => {
+  const supabase = createServerComponentClient({ cookies });
+
+  if (is_verified.toLowerCase() == "pending") {
+    const { error } = await supabase
+      .from("journal_publications")
+      .update({
+        paper_title: paper_title,
+        journal_name: journal_name,
+        issn_number: issn_number,
+        month_and_year_of_publication: date_of_publication,
+        indexed_in: indexed_in,
+        link: link,
+      })
+      .eq("id", id);
+  } else {
+    const { error } = await supabase
+      .from("journal_publications")
+      .update({
+        paper_title: paper_title,
+        journal_name: journal_name,
+        issn_number: issn_number,
+        month_and_year_of_publication: date_of_publication,
+        indexed_in: indexed_in,
+        link: link,
+        is_verified: "Pending",
+      })
+      .eq("id", id);
+  }
+};
+
+export const updatePatents = async (
+  patent_name: string,
+  patent_date: string,
+  patent_type: string,
+  application_no: string,
+  status: string,
+  patent_link: string,
+  is_verified: string,
+  id: number
+) => {
+  const supabase = createServerComponentClient({ cookies });
+
+  if (is_verified.toLowerCase() == "pending") {
+    const { error } = await supabase
+      .from("patents")
+      .update({
+        patent_name: patent_name,
+        patent_date: patent_date,
+        patent_type: patent_type,
+        application_no: application_no,
+        status: status,
+        patent_link: patent_link,
+      })
+      .eq("id", id);
+  } else {
+    const { error } = await supabase
+      .from("patents")
+      .update({
+        patent_name: patent_name,
+        patent_date: patent_date,
+        patent_type: patent_type,
+        application_no: application_no,
+        status: status,
+        patent_link: patent_link,
+        is_verified: "Pending",
+      })
+      .eq("id", id);
+  }
+};
+
+export const updateWorkshops = async (
+  date: string,
+  type: string,
+  title: string,
+  number_of_days: number,
+  // organized_by: string,
+  is_verified: string,
+  id: number
+) => {
+  const supabase = createServerComponentClient({ cookies });
+
+  if (is_verified.toLowerCase() == "pending") {
+    const { error } = await supabase
+      .from("fdp_workshop_refresher_course")
+      .update({
+        date: date,
+        type: type,
+        title: title,
+        number_of_days: number_of_days,
+        // organized_by: organized_by,
+      })
+      .eq("id", id);
+  } else {
+    const { error } = await supabase
+      .from("fdp_workshop_refresher_course")
+      .update({
+        date: date,
+        type: type,
+        title: title,
+        number_of_days: number_of_days,
+        // organized_by: organized_by,
+        is_verified: "Pending",
+      })
+      .eq("id", id);
+  }
+};
+
+//have first argument as primary key ie the email which is not editable. All others update variables can go next add more args as needed
+export const updateUserWhenLoggedIn = async (
+  email: string | undefined,
+  phone: string
+) => {
+  const supabase = createServerComponentClient({ cookies });
+  const { error } = await supabase
+    .from("faculty")
+    .update({ faculty_phone: phone })
+    .eq("faculty_email", email);
+  if (error) {
+    console.log("Error from update user: ", error.message);
+  }
+};
 
 export async function conferenceQuery(
   filterByDateRange: null | boolean = null,
@@ -218,7 +411,7 @@ export async function conferenceQuery(
   end_date: null | string = new Date().toJSON().slice(0, 10),
   filterByType: null | boolean = null,
   type: null | string = null,
-  filterByVerified: null | boolean=null,
+  filterByVerified: null | boolean = null,
   verified: null | string = "PENDING"
 ) {
   const cookieStore = cookies();
@@ -321,12 +514,11 @@ export async function fdpQuery(
   }
 }
 
-
 export async function journalQuery(
   filterByDateRange: null | boolean = null,
   start_date: null | string = "2001-01-01",
   end_date: null | string = new Date().toJSON().slice(0, 10),
-  filterByName: null | boolean = null,  // Journal name
+  filterByName: null | boolean = null, // Journal name
   name: null | string = null,
   filterByIndexedIn: null | boolean = null,
   indexedIn: null | string = "",
@@ -380,19 +572,18 @@ export async function journalQuery(
   }
 }
 
-
 export async function patentsQuery(
   filterByDateRange: null | boolean = null,
   start_date: null | string = "2001-01-01",
   end_date: null | string = new Date().toJSON().slice(0, 10),
-  filterByName: null | boolean = null,  // Patent name
+  filterByName: null | boolean = null, // Patent name
   name: null | string = null,
   filterByType: null | boolean = null,
   type: null | string = null,
   filterByStatus: null | boolean = null,
   status: null | string = "",
   filterByApplicationNo: null | boolean = null,
-  applicationNo: null | string = "",	
+  applicationNo: null | string = "",
   filterByIndexedIn: null | boolean = null,
   indexedIn: null | string = "",
   filterByTitle: null | boolean = null, //Paper title
@@ -409,8 +600,7 @@ export async function patentsQuery(
       .select()
       .gte("date", start_date)
       .lte("date", end_date);
-      return patents;
-
+    return patents;
   }
 
   if (filterByName) {
@@ -423,25 +613,25 @@ export async function patentsQuery(
 
   if (filterByType) {
     const { data: patents } = await supabase
-    .from("patents")
-    .select()
-    .eq("patent_type", type);	
+      .from("patents")
+      .select()
+      .eq("patent_type", type);
     return patents;
   }
 
   if (filterByStatus) {
     const { data: patents } = await supabase
-    .from("patents")
-    .select()
-    .eq("status", status);	
+      .from("patents")
+      .select()
+      .eq("status", status);
     return patents;
   }
 
   if (filterByApplicationNo) {
     const { data: patents } = await supabase
-    .from("patents")
-    .select()
-    .eq("application_no", applicationNo);	
+      .from("patents")
+      .select()
+      .eq("application_no", applicationNo);
     return patents;
   }
 
@@ -469,4 +659,3 @@ export async function patentsQuery(
     return patents;
   }
 }
-
