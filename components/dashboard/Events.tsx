938dc086f68b093d5c5172f989a05289683c9d0b
Adelicia Sequeira <a.seq2804@gmail.com>
2023-10-23 15:45:43 +0530

    added new routes: modify & report

--- components/dashboard/Events.tsx
+++ components/dashboard/Events.tsx
@@ -12,38 +12,52 @@ const bodyText = Urbanist({
 
 export default function Events() {
   return (
-    <div className="h-fit bg-teal-500/20 lg:p-8 lg:mt-6 col-span-2 row-start-2 border border-transparent rounded">
+    <div className="h-fit bg-teal-500/20 lg:p-8 lg:mt-10 col-span-2 row-start-2 border border-transparent rounded">
       <h2 className={`${headerText.className} tracking-wide text-center font-bold uppercase lg:text-2xl text-teal-950 pb-6`}>
-        Your Activity
+        Your Feed
       </h2>
 
-      <ul className={`${bodyText.className} text-teal-950 bg-teal-700/20 lg:p-16 lg:pt-12 flex flex-row justify-between items-center gap-16 border border-transparent rounded uppercase tracking-wider`}>
+      <ul className={`${bodyText.className} text-teal-950 bg-teal-700/20 lg:px-16 lg:py-12 flex flex-row justify-between items-center gap-8 border border-transparent rounded uppercase tracking-wider`}>
         <a href="/my-workshops">
           <li
-            className={`bg-teal-300 w-[10vw] py-6 text-center shadow-lg shadow-teal-600/60 border-[2.5px] border-transparent hover:border-cyan-800 rounded-tl-md rounded-tr-xl rounded-bl-xl rounded-br-md`}
+            className={`bg-teal-300 w-[9vw] py-4 text-center shadow-lg shadow-teal-600/60 border-[2.5px] border-transparent hover:border-cyan-800 rounded-tl-md rounded-tr-xl rounded-bl-xl rounded-br-md`}
           >
             Workshops
           </li>
         </a>
         <a href="/my-conferences">
           <li
-            className={`bg-teal-300 w-[10vw] py-6 text-center shadow-lg shadow-teal-600/60 border-[2.5px] border-transparent hover:border-cyan-800 rounded-tl-md rounded-tr-xl rounded-bl-xl rounded-br-md`}
+            className={`bg-teal-300 w-[9vw] py-4 text-center shadow-lg shadow-teal-600/60 border-[2.5px] border-transparent hover:border-cyan-800 rounded-tl-md rounded-tr-xl rounded-bl-xl rounded-br-md`}
           >
             Conferences
           </li>
         </a>
         <a href="/my-journals">
           <li
-            className={`bg-teal-300 w-[10vw] py-6 text-center shadow-lg shadow-teal-600/60 border-[2.5px] border-transparent hover:border-cyan-800 rounded-tl-md rounded-tr-xl rounded-bl-xl rounded-br-md`}
+            className={`bg-teal-300 w-[9vw] py-4 text-center shadow-lg shadow-teal-600/60 border-[2.5px] border-transparent hover:border-cyan-800 rounded-tl-md rounded-tr-xl rounded-bl-xl rounded-br-md`}
           >
             Journals
           </li>
         </a>
         <a href="/my-patents">
           <li
-            className={`bg-teal-300 w-[10vw] py-6 text-center shadow-lg shadow-teal-600/60 border-[2.5px] border-transparent hover:border-cyan-800 rounded-tl-md rounded-tr-xl rounded-bl-xl rounded-br-md`}
+            className={`bg-teal-300 w-[9vw] py-4 text-center shadow-lg shadow-teal-600/60 border-[2.5px] border-transparent hover:border-cyan-800 rounded-tl-md rounded-tr-xl rounded-bl-xl rounded-br-md`}
           >
             Patents
+          </li>
+        </a>
+        <a href="/modify">
+          <li
+            className={`bg-teal-300 w-[9vw] py-4 text-center shadow-lg shadow-teal-600/60 border-[2.5px] border-transparent hover:border-cyan-800 rounded-tl-md rounded-tr-xl rounded-bl-xl rounded-br-md`}
+          >
+            Modify
+          </li>
+        </a>
+        <a href="/report-gen">
+          <li
+            className={`bg-teal-300 w-[9vw] py-4 text-center shadow-lg shadow-teal-600/60 border-[2.5px] border-transparent hover:border-cyan-800 rounded-tl-md rounded-tr-xl rounded-bl-xl rounded-br-md`}
+          >
+            Reports
           </li>
         </a>
       </ul>
--- components/nav/NavTwo.tsx
+++ components/nav/NavTwo.tsx
@@ -16,7 +16,7 @@ export default function NavTwo() {
       {/* Navigation Bar - 2 */}
       <ul
         id="navbar-2-elements"
-        className="uppercase tracking-wider h-full lg:text-sm sm:text-[0.60rem] grid grid-cols-1 grid-rows-6 sm:gap-2 lg:gap-[1.5rem] items-center justify-between text-center"
+        className="uppercase tracking-wider h-full lg:text-sm sm:text-[0.60rem] grid grid-cols-1 grid-rows-8 gap-4 justify-around items-center text-center"
       >
         <li>
           <a href="/">
@@ -24,7 +24,7 @@ export default function NavTwo() {
               id="logo-home"
               src={logoImg}
               alt="Logo Image"
-              className="mx-auto lg:w-4/6 lg:h-4/6 md:3/5 md:3/5 w:2/5 h:2/5 pt-2"
+              className="mx-auto -mt-[10%] -mb-6 lg:w-4/6 lg:h-4/6 md:3/5 md:3/5 w:2/5 h:2/5"
             />
           </a>
         </li>
@@ -36,7 +36,7 @@ export default function NavTwo() {
               viewBox="0 0 24 24"
               strokeWidth={1.5}
               stroke="currentColor"
-              className="mx-auto w-[2.5rem] h-[2.5rem] sm:w-[40px] sm:h-[40px] md:w-[45px] md:h-[45px] lg:w-[50px] lg:h-[50px]"
+              className="mx-auto w-[2.5rem] h-[2.5rem] sm:w-[40px] sm:h-[40px] md:w-[45px] md:h-[45px] lg:w-[45px] lg:h-[45px]"
             >
               <path
                 strokeLinecap="round"
@@ -55,7 +55,7 @@ export default function NavTwo() {
               viewBox="0 0 24 24"
               strokeWidth={1.5}
               stroke="currentColor"
-              className="mx-auto w-[2.5rem] h-[2.5rem] sm:w-[40px] sm:h-[40px] md:w-[45px] md:h-[45px] lg:w-[50px] lg:h-[50px]"
+              className="mx-auto w-[2.5rem] h-[2.5rem] sm:w-[40px] sm:h-[40px] md:w-[45px] md:h-[45px] lg:w-[45px] lg:h-[45px]"
             >
               <path
                 strokeLinecap="round"
@@ -74,7 +74,7 @@ export default function NavTwo() {
               viewBox="0 0 24 24"
               strokeWidth={1.5}
               stroke="currentColor"
-              className="mx-auto w-[2.5rem] h-[2.5rem] sm:w-[40px] sm:h-[40px] md:w-[45px] md:h-[45px] lg:w-[50px] lg:h-[50px]"
+              className="mx-auto w-[2.5rem] h-[2.5rem] sm:w-[40px] sm:h-[40px] md:w-[45px] md:h-[45px] lg:w-[45px] lg:h-[45px]"
             >
               <path
                 strokeLinecap="round"
@@ -93,7 +93,7 @@ export default function NavTwo() {
               viewBox="0 0 24 24"
               strokeWidth={1.5}
               stroke="currentColor"
-              className="mx-auto w-[2.5rem] h-[2.5rem] sm:w-[40px] sm:h-[40px] md:w-[45px] md:h-[45px] lg:w-[50px] lg:h-[50px]"
+              className="mx-auto w-[2.5rem] h-[2.5rem] sm:w-[40px] sm:h-[40px] md:w-[45px] md:h-[45px] lg:w-[45px] lg:h-[45px]"
             >
               <path
                 strokeLinecap="round"
@@ -112,7 +112,7 @@ export default function NavTwo() {
               viewBox="0 0 24 24"
               strokeWidth={1.5}
               stroke="currentColor"
-              className="mx-auto w-[2.5rem] h-[2.5rem] sm:w-[40px] sm:h-[40px] md:w-[45px] md:h-[45px] lg:w-[50px] lg:h-[50px]"
+              className="mx-auto w-[2.5rem] h-[2.5rem] sm:w-[40px] sm:h-[40px] md:w-[45px] md:h-[45px] lg:w-[45px] lg:h-[45px]"
             >
               <path
                 strokeLinecap="round"
@@ -121,6 +121,55 @@ export default function NavTwo() {
               />
             </svg>
             <p className=" ">Patents</p>
+          </a>
+        </li>
+
+        {/* should only be accessible by hod  */}
+        <li className="hover:text-emerald-300 ">
+          <a href="/modify">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="mx-auto w-[2.5rem] h-[2.5rem] sm:w-[40px] sm:h-[40px] md:w-[45px] md:h-[45px] lg:w-[45px] lg:h-[45px]"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21.75 6.75a4.5 4.5 0 01-4.884 4.484c-1.076-.091-2.264.071-2.95.904l-7.152 8.684a2.548 2.548 0 11-3.586-3.586l8.684-7.152c.833-.686.995-1.874.904-2.95a4.5 4.5 0 016.336-4.486l-3.276 3.276a3.004 3.004 0 002.25 2.25l3.276-3.276c.256.565.398 1.192.398 1.852z"
+              />
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4.867 19.125h.008v.008h-.008v-.008z"
+              />
+            </svg>
+
+            <p className=" ">Modify</p>
+          </a>
+        </li>
+
+        {/* should only be accessible by hod & editor */}
+        <li className="hover:text-emerald-300 ">
+          <a href="/report-gen">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="mx-auto w-[2.5rem] h-[2.5rem] sm:w-[40px] sm:h-[40px] md:w-[45px] md:h-[45px] lg:w-[45px] lg:h-[45px]"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25M9 16.5v.75m3-3v3M15 12v5.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
+              />
+            </svg>
+
+            <p className=" ">Reports</p>
           </a>
         </li>
       </ul>
